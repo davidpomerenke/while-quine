02cd76608c0312c9ec285cae9fe5ae5b8e3ddac5
David Pomerenke <david.pomerenke@uni-konstanz.de>
2020-03-10 02:49:52 +0000

    Compute nearest ASTs for all states.

--- index.mjs
+++ index.mjs
@@ -9,17 +9,20 @@ export const whileQuineProblem = (config = {}) => {
   config = {
     historyFileName: 'history', // .csv
     tempFileName: 'test', // .while
-    timeout: 5, // seconds to wait for program termination
+    timeout: 10, // seconds to wait for program termination
     verbose: true, // log program codes
     ...config
   }
+  // read precomputed data from file
   const history = config.historyFileName
     ? new Map(
       parseCSV(fs.readFileSync(config.historyFileName + '.csv', { encoding: 'utf-8', flag: 'a+' }))
         .map(a => [a[0], a.slice(1)]))
     : new Map()
-  const nonterminal = /<[^>]+>/
-  const variables = state => state.match(/X\d+/g)
+  const nonterminal = /<[^>]+>/ // matches the first empty place
+  const variables = state => state.match(/X\d+/g) // list of all variables in code
+  // compute abstract syntax tree of code and its generated output
+  // or load from table if already computed before
   const AST = code => {
     const hash = sha3(code).toString()
     if (history.has(hash)) {
@@ -30,16 +33,20 @@ export const whileQuineProblem = (config = {}) => {
       fs.writeFileSync(config.tempFileName + '.while', code)
       const exec = a => childProcess.execSync(a, { encoding: 'utf-8' }).replace(/\s|\n|\b/g, '')
       const programAST = exec(`./hwhile -u ${config.tempFileName}.while`)
-      const resultAST = exec(`timeout ${config.timeout} ./hwhile -La ${config.tempFileName}.while nil || echo "timeout:${config.timeout}s"`)
+      const outputAST = exec(`timeout ${config.timeout} ./hwhile -La ${config.tempFileName}.while nil || echo "timeout:${config.timeout}s"`)
       // store results
-      const result = [programAST, resultAST]
+      const result = [programAST, outputAST]
       history.set(hash, result)
       fs.appendFileSync(config.historyFileName + '.csv', [hash, ...result].map(a => `"${a}"`).join(',') + '\n')
       return result
     }
   }
   return new SearchProblem({
-    initialState: { code: config.tempFileName + ' read IgnoreInput {<BLOCK>} write X1' },
+    initialState: { 
+      code: config.tempFileName + ' read IgnoreInput {<BLOCK>} write X1',
+      programAST: '[0,[],1]',
+      outputAST: '0'
+    },
     actions: state => state.code.match(nonterminal)
       ? { // simplified grammar, e. g., empty blocks are omitted
         '<EXPRESSION>': [
@@ -67,19 +74,17 @@ export const whileQuineProblem = (config = {}) => {
       : [],
     result: (state, action) => {
       const code = state.code.replace(nonterminal, action)
-      if (!code.match(nonterminal)) {
-        const [programAST, resultAST] = AST(code)
-        return { code: code, programAST: programAST, resultAST: resultAST }
-      } else return { code: code }
-    },
-    goalTest: state => 'programAST' in state && state.programAST === state.resultAST,
-    stepCost: (state, action) => (action.match(new RegExp(nonterminal.source, 'g')) || []).length,
-    heuristic: state => editDistance.get(
-      ...AST(
+      // fill all empty places and get abstract syntax tree of program and output
+      const [programAST, outputAST] = AST(
         state.code
           .replace(/<BLOCK>/g, '<COMMAND>')
           .replace(/<COMMAND>/g, '<VARIABLE> := <EXPRESSION>')
           .replace(/<VARIABLE>/g, 'X1')
-          .replace(/<EXPRESSION>/g, 'nil')))
+          .replace(/<EXPRESSION>/g, 'nil'))
+      return { code: code, programAST: programAST, outputAST: outputAST }
+    },
+    goalTest: state => state.programAST === state.outputAST,
+    stepCost: (state, action) => (action.match(new RegExp(nonterminal.source, 'g')) || []).length, // number of empty places
+    heuristic: state => editDistance.get(state.programAST, state.outputAST)
   })
 }
